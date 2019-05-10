ee2f2b042d676601149d6a47dfd237a619aef3b5
Farhat Binte Azam <33277216+FarhatBuet14@users.noreply.github.com>
2019-05-11 02:00:03 +0600

    Add files via upload

---
+++ Arduino Codes/Codes with Hands/Line_Follower_Codes/Sonar_Sensor.h
@@ -0,0 +1,14 @@
+#ifndef __SONAR_SENSOR_H__ //Must Needed
+#define __SONAR_SENSOR_H__
+#include "Arduino.h"
+class Sonar
+{
+  public:
+  int trigPin;
+  int echoPin;
+  
+  Sonar();
+  void SetSonar(int TrigPin, int EchoPin);
+  float measure();
+};
+#endif // Must be given
---
+++ Arduino Codes/Final Codes/Line_Follower_Codes/Sonar_Sensor.h
@@ -0,0 +1,14 @@
+#ifndef __SONAR_SENSOR_H__ //Must Needed
+#define __SONAR_SENSOR_H__
+#include "Arduino.h"
+class Sonar
+{
+  public:
+  int trigPin  = 0;
+  int echoPin = 0;
+  
+  Sonar();
+  void SetSonar(int TrigPin, int EchoPin);
+  float measure();
+};
+#endif // Must be given
